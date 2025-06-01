fb8879d8c25bc2c96cf2788702254e3190719f9f
Tanvirul Islam <94637393+originalvondo@users.noreply.github.com>
2025-06-01 21:56:38 +0600

    added workflow

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -16,7 +16,7 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4 text-gray-900 dark:text-white">404</h1>
         <p className="text-xl text-gray-600 dark:text-gray-400 mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 underline">
+        <a href={`${__BASE_URL__}`} className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 underline">
           Return to Home
         </a>
       </div>
