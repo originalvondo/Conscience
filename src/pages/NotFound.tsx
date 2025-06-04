a723ae23de4a4a5c9ab83f8961e74319fe4dc31b
Tanvirul Islam <94637393+originalvondo@users.noreply.github.com>
2025-06-04 22:14:25 +0600

    fixed 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   const location = useLocation();
@@ -16,9 +17,9 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4 text-gray-900 dark:text-white">404</h1>
         <p className="text-xl text-gray-600 dark:text-gray-400 mb-4">Oops! Page not found</p>
-        <a href={`${__BASE_URL__}`} className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 underline">
+        <Link to={`${__BASE_URL__}`} className="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 underline">
           Return to Home
-        </a>
+        </Link>
       </div>
     </div>
   );
